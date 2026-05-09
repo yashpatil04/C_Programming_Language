9f7064260262b476c0e6f46d35dccbbf6bb4e045
Yash Patil <yashpatil092003@gmail.com>
2026-05-09 15:15:42 +0530

    Add files via upload

---
+++ Premitive.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    char ch = 'A';
+    int i = 11;
+    float f = 90.99f;
+    double d = 78.67890;
+
+    printf("R values of all variables\n");
+    printf("%c\n", ch);
+    printf("%d\n",i);
+    printf("%f\n",f);
+    printf("%lf\n",d);
+
+
+    return 0;
+}
---
+++ open_file.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include<unistd.h>
+
+int main()
+{
+    int fd = 0;
+
+    fd = open("Marvellous.txt", O_RDWR);
+
+    if(fd == -1)
+    {
+        printf("Unable to crate file");
+    }
+
+    else
+    {
+        printf("File gets Open with fd : %d\n", fd);
+    }
+
+
+    return 0;
+}
